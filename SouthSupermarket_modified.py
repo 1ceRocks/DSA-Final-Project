d9a09f6844e69d24294ae1766cf4ec8db233e497
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-02-12 23:06:35 +0800

    Program Configuration on Shopping Grocery Items

--- SouthSupermarket_modified.py
+++ SouthSupermarket_modified.py
@@ -1,5 +1,7 @@
 #  Licensed under the Apache License, Version 2.0 (the "License");
 
+import os
+
 #? dictionary is used to store collection of data in the format of key:value pairs that are useful for filtering values that connects two different elements (grocery items and its price). 
 itemAvailableDict = {}
 
@@ -32,43 +34,58 @@ my_file.close()
 print("***********Items Available in Our Store****************")
 
 #? for loop through the list in appending the element to a dictionary (itemAvailableDict)
-for item in itemsAvailable:
-   item_name = item.split()[0] #? split[0] to obtain string and store to its assigned local variable
-   item_price = item.split()[1] #? split[1] to obtain string and store to its assigned local variable
-   print(f"{item_name}: {item_price}") #? display the item name and price attribute in the program for console purposes.
+def main_menu():
+    for item in itemsAvailable:
+        item_name = item.split()[0] #? split[0] to obtain string and store to its assigned local variable
+        item_price = item.split()[1] #? split[1] to obtain string and store to its assigned local variable
+        print(f"{item_name}: {item_price}") #? display the item name and price attribute in the program for console purposes.
+        
+        #? this line is essential as it needs to reserve the elements accurately right in key:value order for accessing the program from the user interface input (shoppingDict).
+        itemAvailableDict.update({item_name: float(item_price)})
    
-   #? this line is essential as it needs to reserve the elements accurately right in key:value order for accessing the program from the user interface input (shoppingDict).
-   itemAvailableDict.update({item_name: float(item_price)})
-   
+main_menu()
 print("*" * 20)
 # print(itemAvailableDict)
 #prompt user to add items
-proceedShopping = input(" \bDo you wish to proceed? (yes / no) \n>>> ")
 shopping_Cart = True
 
 #? executes the command and returns a list of items available for the user to purchase if it wants to proceed shopping for a particular item.
-while shopping_Cart:
-    item_added = input("Add an item: ")
-  
-    if item_added.title() in itemAvailableDict:
-        item_qty = int(input("Add quantity: "))
-        shoppingDict.update({item_added:{"quantity":item_qty,"subtotal":itemAvailableDict[item_added.title()]*item_qty}})
-        print(shoppingDict)
-    
-    elif item_added.isdigit() or item_added.isdecimal() or item_added.isspace():
-        print("Please input a correct feed into our program.")
-    
-    else:
-        print("unable to add unavailable item")
-        
-    proceedShopping = input("Do you wish to add more items (yes/no): ")
-    
-    try:
-        if proceedShopping.lower() == "no":
-            shopping_Cart = False
+
+def purchaseItems():
+    while shopping_Cart:
+        os.system('cls')
+        proceedShopping = input(" \bDo you wish to proceed? (yes / no) \n>>> ")
+        try:
+            if proceedShopping.lower() == "no":
+                shopping_Cart = False
+                
+        except ValueError:
+            print('Please input a correct feed into our program.')
             
-    except ValueError:
-        print('Please input a correct feed into our program.')
+        
+        def browseItems():
+            while True:
+                item_added = input("Add an item: ")
+            
+                if item_added.title() in itemAvailableDict:
+                    item_qty = int(input("Add quantity: "))
+                    shoppingDict.update({item_added:{"quantity":item_qty,"subtotal":itemAvailableDict[item_added.title()]*item_qty}})
+                    print(shoppingDict)
+                
+                elif item_added.isdigit() or item_added.isdecimal() or item_added.isspace():
+                    print("Please input a correct feed into our program.")
+                
+                else:
+                    print("unable to add unavailable item")
+            
+        continueShopping = input("Do you wish to add more items? (yes / no) \n>>> ")
+        
+        if 
+
+purchaseItems()
+continueShopping = input("Do you wish to add more items (yes/no): ")
+
+
   
 #? prints out the total amount of items the user shopped from the global user variable shoppingDict{} with the summary of total prices aligned to quantity input.
 else:
