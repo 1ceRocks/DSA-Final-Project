90c42d15f9a1a52cefce7741abc2f881bdfd16ff
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-02-15 02:20:08 +0800

    Background Image Supplemented
    To provide a more appealing design on the front-end between the consumers who wish to buy using an electronic-network based supermarket GUI, I supplemented a background image for the program.

--- SouthSupermarket_GUI.py
+++ SouthSupermarket_GUI.py
@@ -4,3 +4,11 @@ root = Tk()
 root.geometry("1500x570")
 root.title("VILLARIZA South Supermarket")
 root.resizable(False, False)
+
+pyCanvas = Canvas(root, bg = "gray16", height = 200, width = 200)
+imageFile = PhotoImage(file = "")
+
+FRAME = Frame(root, bg="blue", height=100, width=300)
+imageFile = PhotoImage(file = "supermarket.png")
+background_label = Label(root, image = imageFile, anchor="center")
+background_label.place(x=0, y=0, relwidth = 1, relheight = 1)
