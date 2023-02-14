3638984e1605ae9f0754658d3dc567e1f82b6258
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-02-15 02:21:50 +0800

    Background Image Error Occurred
    The relative path of the image supermarket.jpg cannot be read and opened therefore I temporarily store the code blocks responsible for image rendering as a comment line.

--- SouthSupermarket_GUI.py
+++ SouthSupermarket_GUI.py
@@ -5,15 +5,15 @@ root.geometry("1500x570")
 root.title("VILLARIZA South Supermarket")
 root.resizable(False, False)
 
-pyCanvas = Canvas(root, bg = "gray16", height = 200, width = 200)
-imageFile = PhotoImage(file = "")
+# pyCanvas = Canvas(root, bg = "gray16", height = 200, width = 200)
+# imageFile = PhotoImage(file = "")
 
-FRAME = Frame(root, bg="blue", height=100, width=300)
-imageFile = PhotoImage(file = "supermarket.png")
-background_label = Label(root, image = imageFile, anchor="center")
-background_label.place(x=0, y=0, relwidth = 1, relheight = 1)
+# FRAME = Frame(root, bg="blue", height=100, width=300)
+# imageFile = PhotoImage(file = "supermarket.png")
+# background_label = Label(root, image = imageFile, anchor="center")
+# background_label.place(x=0, y=0, relwidth = 1, relheight = 1)
 
 Label(text = "VILLARIZA South Supermarket", bg = "black", fg = "white", font = ("Verdana", 33), width = "300", height = "2").pack()
 
-pyCanvas.pack()
+# pyCanvas.pack()
 root.mainloop()
