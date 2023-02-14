d257226efa3ea9a160aacefa76049f9c46b1f6d5
Fritz Cedrick V. Villariza <villarizaced@gmail.com>
2023-02-15 02:20:52 +0800

    Labeling The Main Title of the Tkinter GUI

--- SouthSupermarket_GUI.py
+++ SouthSupermarket_GUI.py
@@ -12,3 +12,8 @@ FRAME = Frame(root, bg="blue", height=100, width=300)
 imageFile = PhotoImage(file = "supermarket.png")
 background_label = Label(root, image = imageFile, anchor="center")
 background_label.place(x=0, y=0, relwidth = 1, relheight = 1)
+
+Label(text = "VILLARIZA South Supermarket", bg = "black", fg = "white", font = ("Verdana", 33), width = "300", height = "2").pack()
+
+pyCanvas.pack()
+root.mainloop()
